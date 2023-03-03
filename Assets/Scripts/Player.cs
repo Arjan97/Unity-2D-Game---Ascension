2c485185ef52f6b651f47e3d15c37bca5510849a
arjan <arjan.peeters@hva.nl>
2023-03-03 11:50:32 +0100

    Big tings

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,26 +5,71 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //vine swing
     public Vector2 vineVelocityWhenGrabbed;
     Transform currentSwingable;
     ConstantForce2D myConstantForce;
     bool swinging = false;
+    //player movement
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public float dashDistance = 5f;
+    public float dashTime = 0.5f;
 
-    public float speed = 200f; //Controls velocity multiplier
-    private Rigidbody2D rb; //Tells script there is a rigidbody, we can use variable rb to reference it in further script
-    private Vector2 movement;
+    private Rigidbody2D rb;
+    private bool isGrounded = true;
+    private bool isDashing = false;
+    private float horizontalMove;
+    private Vector2 dashDirection;
+    private float scaleX;
+
+    //animations
+    //public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         myConstantForce = GetComponent<ConstantForce2D>();
         rb = GetComponent<Rigidbody2D>(); //rb equals the rigidbody on the player
+        scaleX = transform.localScale.x;
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
         movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        
+        if(Input.GetAxisRaw("Horizontal") != 0)
+        {
+            if(Input.GetAxisRaw("Horizontal") > 0)
+            {
+                anim.Play("Walk");
+            }
+            else
+            {
+                anim.Play("WalkBack");
+            }
+        }
+        else
+        {
+            anim.Play("Idle");
+        } */
 
+        //Player movement init
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+        //dash and jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            isGrounded = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !isDashing)
+        {
+            StartCoroutine(Dash());
+        }
+
+        //player swinging
         if (swinging)
         {
             myConstantForce.enabled = false;
@@ -39,10 +84,62 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.AddForce(movement * speed);
+        //move left and right and flip sprite
+        Move();
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    public void Move()
+    {
+        Flip();
+        rb.velocity = new Vector2(horizontalMove * moveSpeed, rb.velocity.y);
+
+        //dash
+        Vector2 movement = new Vector2(horizontalMove * moveSpeed, rb.velocity.y);
+
+        if (!isDashing)
+        {
+            rb.velocity = movement;
+        }
+        else
+        {
+            rb.velocity = dashDirection * (dashDistance / dashTime);
+        }
+    }
+
+    public void Flip()
+    {
+        if (horizontalMove > 0)
+        {
+            transform.localScale = new Vector3(scaleX, transform.localScale.y, transform.localScale.z);
+        }
+        if (horizontalMove < 0)
+        {
+            transform.localScale = new Vector3((-1) * scaleX, transform.localScale.y, transform.localScale.z);
+        }
+    }
+
+    //Grounded checker
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the player is grounded
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    //Dash routine
+    private IEnumerator Dash()
+    {
+        isDashing = true;
+        dashDirection = new Vector2(Mathf.Sign(horizontalMove), 0f);
+
+        yield return new WaitForSeconds(dashTime);
+
+        isDashing = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Vine")
         {
